713a79ad48c9986c50270192a4ecea265176acc8
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-07-22 13:20:12 +0100

    refactor the add function to be an arrow function

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,8 +2,11 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = const add = (newNeighborhood) => {
+
+  const add = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
+
+  this.addNeighborhood = add;
 }

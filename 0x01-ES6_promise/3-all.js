b2f2b813bd83e038b4608e90cf270df47e3ac723
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-07-23 13:50:48 +0100

    feat: Add handleProfileSignup function
    This commit adds the handleProfileSignup function, which uses the uploadPhoto and createUser functions from the utils module to handle the signup process. It logs the body, firstName, and lastName when the signup is successful, and logs 'Signup system offline' if an error occurs.

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function handleProfileSignup(){
+  try{
+    const { body } = await uploadPhoto()
+    const { firstName, lastName } = await createUser()
+
+    console.log(body, firstName, lastName)
+  }catch{
+    console.log("Signup system offline")
+  }
+}

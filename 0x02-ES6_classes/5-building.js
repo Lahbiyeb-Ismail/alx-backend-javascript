7b86f219378b3cebd3fc539fd469d9a801a5b6b2
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-07-25 14:42:38 +0100

    Implementing a class named Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-underscore-dangle */
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    if (typeof sqft !== 'number') throw new TypeError('Sqft must be a number');
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}

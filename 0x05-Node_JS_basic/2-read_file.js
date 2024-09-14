60c76736675f10d25333ad525ad3cfa6d6f302c8
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-09-14 16:27:49 +0100

    2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,36 +1,36 @@
 const fs = require('fs');
 
 function countStudents(filePath) {
-  try {
-    const fileContent = fs.readFileSync(filePath, 'utf-8').toString().trim();
-    const fileContentLines = fileContent.split('\n').slice(1);
-
-    const numberOfStudents = fileContentLines.length;
-
-    const fieldsMap = {};
-
-    for (const line of fileContentLines) {
-      const studentsInfo = line.split(',');
-      const studentName = studentsInfo[0];
-      const field = studentsInfo[studentsInfo.length - 1];
-
-      if (!fieldsMap[field]) {
-        fieldsMap[field] = { studentsCount: 0, studentsList: [] };
-      }
-
-      fieldsMap[field].studentsCount++;
-      fieldsMap[field].studentsList.push(studentName);
-    }
-
-    console.log(`Number of students: ${numberOfStudents}`);
-    for (const field in fieldsMap) {
-      const { studentsList, studentsCount } = fieldsMap[field];
-      const studentsNum = `Number of students in ${field}: ${studentsCount}.`;
-      const studentsListStr = `List: ${studentsList.join(', ')}`;
-      console.log(`${studentsNum} ${studentsListStr}`);
-    }
-  } catch (error) {
+  if (!fs.existsSync(filePath) || !fs.statSync(filePath).isFile()) {
     throw Error('Cannot load the database');
+  }
+
+  const fileContent = fs.readFileSync(filePath, 'utf-8').toString().trim();
+  const fileContentLines = fileContent.split('\n').slice(1);
+
+  const numberOfStudents = fileContentLines.length;
+
+  const fieldsMap = {};
+
+  for (const line of fileContentLines) {
+    const studentsInfo = line.split(',');
+    const studentName = studentsInfo[0];
+    const field = studentsInfo[studentsInfo.length - 1];
+
+    if (!fieldsMap[field]) {
+      fieldsMap[field] = { studentsCount: 0, studentsList: [] };
+    }
+
+    fieldsMap[field].studentsCount++;
+    fieldsMap[field].studentsList.push(studentName);
+  }
+
+  console.log(`Number of students: ${numberOfStudents}`);
+  for (const field in fieldsMap) {
+    const { studentsList, studentsCount } = fieldsMap[field];
+    const studentsNum = `Number of students in ${field}: ${studentsCount}.`;
+    const studentsListStr = `List: ${studentsList.join(', ')}`;
+    console.log(`${studentsNum} ${studentsListStr}`);
   }
 }
 

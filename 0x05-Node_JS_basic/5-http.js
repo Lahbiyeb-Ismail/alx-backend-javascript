b1ebbdd8c23d471733a2ec510863bb3c60181d76
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-09-15 16:42:54 +0100

    5. Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,40 +1,49 @@
 const http = require('http');
-const fs = require('fs/promises');
+const fs = require('fs');
 
-async function countStudents(filePath) {
-  try {
-    const fileContent = await fs.readFile(filePath, { encoding: 'utf8' });
-    const fileContentLines = fileContent.trim().split('\n').slice(1);
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) reject(new Error('Cannot load the database'));
 
-    const numberOfStudents = fileContentLines.length;
+      if (data) {
+        const fileContent = data
+          .toString('utf-8')
+          .trim()
+          .split('\n');
 
-    const fieldsMap = {};
+        const studentGroups = {};
+        const fieldsName = fileContent[0].split(',');
+        const studentPropNames = fieldsName.slice(0, fieldsName.length - 1);
 
-    for (const line of fileContentLines) {
-      const studentsInfo = line.split(',');
-      const studentName = studentsInfo[0];
-      const field = studentsInfo[studentsInfo.length - 1];
+        for (const line of fileContent.slice(1)) {
+          const studentRecord = line.split(',');
+          const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
 
-      if (!fieldsMap[field]) {
-        fieldsMap[field] = { studentsCount: 0, studentsList: [] };
+          const field = studentRecord[studentRecord.length - 1];
+          if (!Object.keys(studentGroups).includes(field)) {
+            studentGroups[field] = [];
+          }
+
+          const studentEntries = studentPropNames
+            .map((propName, idx) => [propName, studentPropValues[idx]]);
+
+          studentGroups[field].push(Object.fromEntries(studentEntries));
+        }
+
+        const totalStudents = fileContent.length - 1;
+
+        const studentsData = [`Number of students: ${totalStudents}`];
+
+        for (const [field, group] of Object.entries(studentGroups)) {
+          const studentNames = group.map((student) => student.firstname).join(', ');
+          studentsData.push(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
+        }
+
+        resolve(studentsData);
       }
-
-      fieldsMap[field].studentsCount++;
-      fieldsMap[field].studentsList.push(studentName);
-    }
-
-    const data = [`Number of students: ${numberOfStudents}`];
-    for (const field in fieldsMap) {
-      const { studentsList, studentsCount } = fieldsMap[field];
-      const studentsNum = `Number of students in ${field}: ${studentsCount}.`;
-      const studentsListStr = `List: ${studentsList.join(', ')}`;
-      data.push(`${studentsNum} ${studentsListStr}`);
-    }
-
-    return data.join('\n');
-  } catch (error) {
-    throw Error('Cannot load the database');
-  }
+    });
+  });
 }
 
 const filePath = process.argv[2];
@@ -46,9 +55,9 @@ const app = http.createServer(async (req, res) => {
   if (req.url === '/students') {
     res.write('This is the list of our students\n');
 
-    const data = await countStudents(filePath);
-
-    res.end(data);
+    countStudents(filePath).then((data) => {
+      res.end(data.join('\n'));
+    });
   }
 });
 

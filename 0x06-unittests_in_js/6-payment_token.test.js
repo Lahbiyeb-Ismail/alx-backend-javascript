eb27fff3ad6f6a2cb983c070a41ef17b1b448d48
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-09-19 13:28:29 +0100

    6. Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -2,12 +2,11 @@ const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', () => {
-  it('should return a resolved promise with the correct data when success is true', () => new Promise((done) => {
+  it('getPaymentTokenFromAPI(success), where success == true', () => new Promise((done) => {
     getPaymentTokenFromAPI(true)
-      .then((response) => {
-        expect(response).to.deep.equal({ data: 'Successful response from the API' });
+      .then((res) => {
+        expect(res).to.deep.equal({ data: 'Successful response from the API' });
         done();
-      })
-      .catch((err) => done(err));
+      });
   }));
 });

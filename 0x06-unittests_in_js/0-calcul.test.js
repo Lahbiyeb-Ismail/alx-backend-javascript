e5c77d3d7c7a1106eebf74c86e1f76b695d0a729
Lahbiyeb Ismail <ismaillhbibe@gmail.com>
2024-09-19 12:54:10 +0100

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,19 +1,19 @@
-const assert = require("assert")
-const calculateNumber = require("./0-calcul")
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
 
 describe('calcule-test', () => {
   describe('sum of two numbers', () => {
-    it("should return the 4", ()=>{
-      assert.equal(calculateNumber(1, 3), 4)
-    })
-    it("should return the 5", ()=>{
-      assert.equal(calculateNumber(1, 3.7), 5)
-    })
-    it("should return the 5", ()=>{
-      assert.equal(calculateNumber(1.2, 3.7), 5)
-    })
-    it("should return the 6", ()=>{
-      assert.equal(calculateNumber(1.5, 3.7), 6)
-    })
-  })
-})
+    it('should return the 4', () => {
+      assert.equal(calculateNumber(1, 3), 4);
+    });
+    it('should return the 5', () => {
+      assert.equal(calculateNumber(1, 3.7), 5);
+    });
+    it('should return the 5', () => {
+      assert.equal(calculateNumber(1.2, 3.7), 5);
+    });
+    it('should return the 6', () => {
+      assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+  });
+});
